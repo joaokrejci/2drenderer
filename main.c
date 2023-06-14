dcb76a06e60128cda93b3f411e543eaa0788fa0d
João Krejci <joao.krejci@gmail.com>
2023-06-13 21:26:53 -0400

    :fire: removing dynamic array

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #include "2drenderer/src/renderer.h"
 #include "engine/src/game_object.h"
-#include "engine/src/dynamic_array.h"
 
 sprite_t car_sprite;
-dynamic_array_t game_objects;
+struct {
+    game_object_t data[16];
+    int length;
+    int index;
+} objects;
 
 void load() {
-    game_objects = create_dynamic_array(sizeof (game_object_t));
+    objects.length = 0;
+    objects.index = 0;
 
     car_sprite = create_sprite(load_image("../assets/car.png"), 1);
     resize_sprite(&car_sprite, car_sprite.width * 2, car_sprite.height * 2);
@@ -14,26 +18,27 @@ void load() {
 
 void unload() {
     clear_sprite(car_sprite);
-    delete_dynamic_array(&game_objects);
 }
 
 void loop() {
-    game_object_t * object = (game_object_t *) game_objects.data;
-    for (int i = 0; i < game_objects.length; i++) {
-        draw(object->sprite, object->position);
-        object++;
+    for (int i = 0; i < objects.length; i++) {
+        draw(objects.data[i].sprite, objects.data[i].position);
+        i++;
     }
 }
 
 void event_handler(SDL_Event event) {
     if (event.button.button == SDL_BUTTON_LEFT) {
-        game_object_t new_obj;
-        new_obj.sprite = car_sprite;
-        new_obj.position.x = event.button.x;
-        new_obj.position.y = event.button.y;
-        new_obj.height = new_obj.sprite.height;
-        new_obj.width = new_obj.sprite.width;
-        dynamic_array_insert(&game_objects, (unsigned char *) &new_obj, 1);
+        game_object_t * new_obj = &objects.data[objects.index];
+        new_obj->sprite = car_sprite;
+        new_obj->position.x = event.button.x;
+        new_obj->position.y = event.button.y;
+        new_obj->height = new_obj->sprite.height;
+        new_obj->width = new_obj->sprite.width;
+        if(objects.length < 16) {
+            objects.length++;
+        }
+        objects.index = (objects.index + 1) % 16;
     }
 }
 
